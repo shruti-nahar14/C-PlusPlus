724e134b32ecab2c82999b98eb3ea351edaf6603
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-05-30 10:45:20 +0530

    display the message

---
+++ basic.cpp
@@ -0,0 +1,16 @@
+//////////////////////////////////////
+*Input: 0
+*Output: Easytut4u
+i am 18 years old.
+*Description:Display the output
+*Date: 30-may-2021
+*Author: Shruti Nahar 
+/////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=18;
+cout<<"Easytut4u \n";
+cout<<"i am "<<age<<"years old";
+}
