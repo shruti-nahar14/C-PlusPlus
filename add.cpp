29e0361f80081b7bf447cf75128653f99dab7cd7
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-05-30 10:40:42 +0530

    Addition of numbers

---
+++ add.cpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////
+*Input: 10 20
+*Output:  Addition is 30
+*Description:Addition of two numbers
+*Date: 30-may-2021
+*Author: Shruti Nahar 
+/////////////////////////////////////////#include <stdio.h>#include<iostream>
+using namespace std;
+int main()
+{
+int n1,n2,add;
+cout<<"enter two nos ";
+cin>>n1>>n2;
+add=n1+n2;
+cout<<"\n Addition is "<<add;
+return 0;
+}
