53449edacbeea7abf41e6749e465f6167ac20fa5
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-05-30 10:48:31 +0530

    Arithmetic operation on numbers

---
+++ basic.2.cpp
@@ -0,0 +1,24 @@
+//////////////////////////////////////
+*Input:  20 10
+*Output:  addition is 30
+substration is 10
+multiplication is 200
+division is 2
+*Description:Addition,substration,multipliaction & division of two numbers
+*Date: 30-may-2021
+*Author: Shruti Nahar 
+/////////////////////////////////////////
+#include<iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cout<<"enter a value of x";
+cin>>x;
+cout<<"enter a value of y";
+cin>>y;
+cout<<"addition is "<<x+y ;
+cout<<"substraction is "<<x-y  ;
+cout<<"multiplication is "<<x*y;
+cout<<"division is "<<x/y ;
+}
