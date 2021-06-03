2eeedab33dde65a82ed890b07ebc136450c0673f
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-03 09:01:48 +0530

    Do while Loop

---
+++ do while.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////
+/*Input: 0
+*Output: 2 4 6 8 10
+*Description:Using Do While Loop
+*Date: 3-June-2021
+*Author: Shruti Nahar */
+/////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main()
+{
+int i=2;
+do
+{
+cout<<i<<"\n";
+i=i+2;
+}
+while(i<=10);
+}
