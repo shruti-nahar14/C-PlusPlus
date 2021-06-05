aad1eb7601891102cdabaa4e5e032425d11f71d5
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-05 08:21:38 +0530

    Check number is even or odd

---
+++ if else .cpp
@@ -0,0 +1,40 @@
+/////////////////////////////////////
+/*Input: 10
+*Output:even number:10
+*Description:Check the number is even or odd.
+*Date: 5-June-2021
+*Author: Shruti Nahar*/ 
+/////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main()
+{
+int no;
+cout<<"enter a no";
+cin>> no;
+if(no%2==0)
+{
+cout<<"even number:"<<no<<"\n";
+}
+else
+{
+cout<<"odd number: "<<no;
+}
+int time=20;
+if(time<18)
+{
+    cout<<"good morning";
+}
+else
+    {
+  cout<<"good evening \n";
+}
+if(1)
+{
+    cout<<"true block";
+}
+else
+    {
+   cout<<"false block";
+}
+}
