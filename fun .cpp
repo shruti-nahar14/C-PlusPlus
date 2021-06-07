197d48c2ca5b4d13d73ee41b2296ca70c12076e8
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-07 11:09:28 +0530

    Display the message using function

---
+++ fun .cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////
+/*Function:void disp(void);
+*Input:--
+*Output:shruti nahar
+*Description:Display the Message using Function.
+*Date: 7-June-2021
+*Author: Shruti Nahar*/ 
+/////////////////////////////////////////
+#include <iostream>
+using namespace std;
+void disp(void);
+int main()
+{
+disp();
+}
+void disp(void)
+{
+cout<<"shruti nahar";
+}
