457dda07e36936f49800a1482ed6280053ba5e62
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-09 14:32:18 +0530

    Use of Conditional Statement

---
+++ conditional statement.cpp
@@ -0,0 +1,25 @@
+/////////////////////////////////////
+/*Input:5
+*Output:five
+*Description:Enter the number and display its spelling if<9 else display Greater than 9
+*Date: 9-June-2021
+*Author:Shruti Nahar*/ 
+/////////////////////////////////////////
+#include<iostream>
+using namespace std;
+int main() 
+{
+int in;
+string num[10] = {"Greater than 9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+cout<<"Enter the number";
+cin >> in;
+
+if(in > 9){
+    cout << num[0];
+}
+else{
+    cout << num[in];
+}
+
+return 0;
+}
