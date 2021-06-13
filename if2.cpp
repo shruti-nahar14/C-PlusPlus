7595d883d50db042770abe2b4161cdd1d7258f1f
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-13 09:17:00 +0530

    Use of IF STATEMENT to find the greater number

---
+++ if2.cpp
@@ -0,0 +1,30 @@
+/////////////////////////////////////
+/*Input: enter a value of x 10
+         enter a value of y 3
+*Output:x is greater than y
+        value of n is 10
+        end of program
+*Description:Use of IF STATEMENT to find greater number
+*Date: 13-June-2021
+*Author: Shruti Nahar*/ 
+/////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cout<<"enter a value of x";
+cin>>x;
+cout<<"enter a value of y";
+cin>>y;
+if(x>y)
+{
+cout<<"x is greater than y \n";
+}
+int n=1;
+if(n)
+{
+    cout<<"value of n is"<<n<<"\n";
+}
+cout<<"end of program";
+}
