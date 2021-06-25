571087889c580fe912fadd7b4aa62123dcd20073
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-25 10:41:54 +0530

    Volume of Sphere

---
+++ volume of sphere.cpp
@@ -0,0 +1,18 @@
+//////////////////////////////////////
+/*Input: enter a radius 10
+*Output: volume of sphere is 4082
+*Description:Find the Volume of Sphere
+*Date: 25-June-2021
+*Author: Shruti Nahar */
+/////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main()
+{
+int r;
+float v;
+cout<<"enter a radius ";
+cin>>r;
+v=1.3*3.14*(r*r*r);
+cout<<"volume of sphere is "<<v;
+}
