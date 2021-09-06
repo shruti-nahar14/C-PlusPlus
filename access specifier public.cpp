32bb71b110cbfd2fe53cf49055bc4a971fb795bd
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-06 08:34:07 +0530

    Implementation of public access specifier

---
+++ access specifier public.cpp
@@ -0,0 +1,58 @@
+/////////////////////////////////////
+/*Input: 1 2 3 4 5
+*Output: 
+         Base class
+         value of A in base clase 10
+         value of B in base class 20
+         value of A in base clase 10
+         value of B in base class 20
+         in main
+         value of B in base class 200
+*Description:Implementation of public access specifier
+*Date:06-09-2021
+*Author: Shruti Nahar */
+/////////////////////////////////////////
+#include<iostream>
+using namespace std;
+class A
+{
+    protected:
+    int x;
+    public:
+    int y;
+    public:
+    A()
+    {
+        cout<<"\nBase class";
+        x=10;
+        y=20;
+    }
+};
+class B:public A
+{
+    public:
+    B()
+    {
+        cout<<"\nvalue of A in base clase "<<x;
+        cout<<"\nvalue of B in base class "<<y;
+    }
+};
+class c:public B
+{
+    public:
+    c()
+    {
+        cout<<"\nvalue of A in base clase "<<x;
+        cout<<"\nvalue of B in base class "<<y;
+    }
+};
+int main()
+{
+    c obj;
+    //obj.x=100;
+    obj.y=200;
+    cout<<"\n in main ";
+     //cout<<"\nvalue of A in base clase "<<x;
+    cout<<"\nvalue of B in base class "<<obj.y;
+
+}
