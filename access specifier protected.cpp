45a14dc302ce3f1f227e54d2b256f37be73b3948
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-09 22:10:49 +0530

    Update access specifier protected.cpp

--- access specifier protected.cpp
+++ access specifier protected.cpp
@@ -9,7 +9,7 @@
 *Description:Implementation of protected access specifier
 *Date:06-09-2021
 *Author: Shruti Nahar */
-/////////////////////////////////////////
+//////////////////////////////////////////
 #include<iostream>
 using namespace std;
 class A
