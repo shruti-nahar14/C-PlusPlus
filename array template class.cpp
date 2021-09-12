689992e94f9e1e03757416d6b6942f447c979637
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-12 16:03:24 +0530

    Add files via upload

---
+++ array template class.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#define MAX 5 
+template <class T>
+class mygreater
+{
+public:
+T arr[MAX],i;
+T greaterarr()
+{
+for(i=0;i<MAX;i++)
+{
+if(arr[i]<arr[i+1])
+{
+return arr[i+1];
+}
+else
+{
+return arr[i];
+}
+}
+}
+};
+int main()
+{
+T arr[MAX],i;
+for(i=0;i<MAX;i++)
+{
+cout<<arr[i];
+}
+mygreater <int> myobject;
+cout<<myobject.greaterarr();
+}
